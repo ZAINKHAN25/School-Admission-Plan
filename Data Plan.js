4e98ca45e66cb99bfcd901490c26463fea391d31
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-12-09 19:18:35 +0500

    First commit

---
+++ Data Plan.js
@@ -0,0 +1,66 @@
+Students = [
+	{
+		Name: "Zain",
+		Age: 15,
+		FatherName: "Anwar Saeed",
+		AdmissionDate: "10/5/2022",
+		AdmissionClass: 8,
+		CurrentFees: 3800,
+		CurrentClass: 9,
+		StudentDescription: "It is a very good boy",
+		FeesPaid: [
+			{
+				MonthName: "January",
+				YearName: 2022,
+				MonthFees: 3800
+			}, {
+				MonthName: "Febrary",
+				YearName: 2022,
+				MonthFees: 3800,
+			},
+		],
+		FeesUnpaid: [
+			{
+				MonthName: "January",
+				YearName: 2022,
+				MonthFees: 3800,
+			}, {
+				MonthName: "Febrary",
+				YearName: 3800,
+				MonthFees: 2022,
+			},
+		],
+	},
+	{
+		Name: "Talha",
+		Age: 14,
+		FatherName: "Hammad Anwar",
+		AdmissionDate: "8/10/2012",
+		AdmissionClass: 1,
+		CurrentFees: 3800,
+		CurrentClass: 9,
+		StudentDescription: "It is a very good boy",
+		FeesPaid: [
+			{
+				MonthName: "January",
+				YearName: 2022,
+				MonthFees: 3800
+			}, {
+				MonthName: "Febrary",
+				YearName: 2022,
+				MonthFees: 3800,
+			},
+		],
+		FeesUnpaid: [
+			{
+				MonthName: "January",
+				YearName: 2022,
+				MonthFees: 3800,
+			}, {
+				MonthName: "Febrary",
+				YearName: 3800,
+				MonthFees: 2022,
+			},
+		],
+	},
+]
